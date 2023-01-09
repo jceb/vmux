aae8fb843780c07f0091f2d6b9aa0799098a700a
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-01-09 19:58:00 +0100

    chore: install shell scripts via setup.py

--- setup.py
+++ setup.py
@@ -5,28 +5,32 @@ from setuptools import setup
 setup(
     name="vmux",
     description="vim/neovim/kakoune session handler within tmux",
-    long_description=open('README.md', 'rt').read().strip(),
-    long_description_content_type='text/markdown',
-    author="Jan Christoph Ebersbach", author_email='jceb@e-jc.de',
+    long_description=open("README.md", "rt").read().strip(),
+    long_description_content_type="text/markdown",
+    author="Jan Christoph Ebersbach",
+    author_email="jceb@e-jc.de",
     url="https://github.com/jceb/vmux",
-    license='GPLv3',
-    packages=['vmux'],
+    license="GPLv3",
+    packages=["vmux"],
     install_requires=[
-        'pynvim>=0.3.2',
+        "pynvim>=0.3.2",
     ],
-    setup_requires=['pytest-runner', 'setuptools_scm'],
-    tests_require=['pytest'],
+    setup_requires=["pytest-runner", "setuptools_scm"],
+    tests_require=["pytest"],
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python',
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
     ],
-    entry_points={
-        'console_scripts': [
-            'vmux = vmux.__main__:main'
-        ]
-    },
-    use_scm_version={
-        "local_scheme": "no-local-version"
-        },
+    entry_points={"console_scripts": ["vmux = vmux.__main__:main"]},
+    scripts=[
+        "scripts/gnvim.vmux",
+        "scripts/gvim.vmux",
+        "scripts/kak.vmux",
+        "scripts/nvim-qt.vmux",
+        "scripts/nvim.vmux",
+        "scripts/nvr.vmux",
+        "scripts/vim.vmux",
+    ],
+    use_scm_version={"local_scheme": "no-local-version"},
     zip_safe=False,
 )
